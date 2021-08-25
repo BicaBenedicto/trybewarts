17353235aa58c64585fb969794cc0c2e31bf4806
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-08-25 18:03:13 -0300

    Ajustes no cypress

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,6 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands';
+import '@david-boydell/cypress-layout';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
